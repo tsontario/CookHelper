5be7104430e32246b129876e396fa5d27f6f62f1
Tim Smith <tsontario@gmail.com>
2016-11-25 10:21:05 -0500

    First Commit

---
+++ app/src/main/java/com/example/caitlin/cookhelper/AddRecipe.java
@@ -0,0 +1,44 @@
+package com.example.caitlin.cookhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar.LayoutParams;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
+
+public class AddRecipe extends AppCompatActivity {
+    /*
+    private LinearLayout instructionLL;
+    private EditText instructionET;
+    private ImageButton addInstructionB;
+    */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recipe);
+
+        /*
+        instructionLL = (LinearLayout) findViewById(R.id.instructionLinearLayout);
+        instructionET = (EditText) findViewById(R.id.firstInstruction);
+        addInstructionB = (ImageButton) findViewById(R.id.addInstructionButton);
+        */
+    }
+    /*
+    public void onAddNewInstructionClick(View view) {
+        int index = instructionLL.indexOfChild(addInstructionB);
+        instructionLL.addView(createNewEditText(),index);
+    }
+
+
+
+    private EditText createNewEditText(){
+        EditText newET = new EditText(this);
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT);
+        newET.setLayoutParams(params);
+        newET.setHint("Enter the next instruction");
+        return newET;
+    }
+    */
+}
--- app/src/main/java/com/example/caitlin/cookhelper/Home.java
+++ app/src/main/java/com/example/caitlin/cookhelper/Home.java
@@ -1,13 +1,13 @@
-package com.roja.tarjeta.cookhelper;
+package com.example.caitlin.cookhelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
