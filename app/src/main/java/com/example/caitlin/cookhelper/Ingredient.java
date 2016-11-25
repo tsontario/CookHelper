090bbcc457a206abd84f9afca3084ff47435c34a
PC <dpetitclerc25@gmail.com>
2016-11-25 14:29:30 -0500

    Ingredient & IngredientMeasure classes

---
+++ app/src/main/java/com/example/caitlin/cookhelper/Ingredient.java
@@ -0,0 +1,14 @@
+package com.example.caitlin.cookhelper;
+
+/**
+ * Created by PC on 2016-11-25.
+ */
+
+public class Ingredient {
+
+    private String name;
+
+    public Ingredient(String name){
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/example/caitlin/cookhelper/IngredientMeasure.java
@@ -0,0 +1,16 @@
+package com.example.caitlin.cookhelper;
+
+/**
+ * Created by PC on 2016-11-25.
+ */
+
+public class IngredientMeasure {
+
+    private String unit;
+    private int amount;
+
+    public IngredientMeasure(String unit, int amount){
+        this.unit = unit;
+        this.amount = amount;
+    }
+}
