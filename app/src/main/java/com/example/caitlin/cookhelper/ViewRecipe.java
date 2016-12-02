3dac1149ac31306042ddeeea887eacc3cf67f376
kim_p <dpetitclerc25@gmail.com>
2016-12-02 16:46:43 -0500

    Code comments + changed Manifest

--- app/src/main/java/com/example/caitlin/cookhelper/ViewRecipe.java
+++ app/src/main/java/com/example/caitlin/cookhelper/ViewRecipe.java
@@ -28,18 +28,17 @@ public class ViewRecipe extends AppCompatActivity {
         Intent intent = getIntent();
         long recipe_id = intent.getLongExtra("recipe_id", 0l);
 
+        //Receiving recipe from RecipeBook façade
         r = rBook.getRecipe(getApplicationContext(), recipe_id);
+        //Populates every View fields
         masterView(r);
-
-        //setting sent recipe name as title
-        //TextView recipeNameToChange = (TextView) findViewById(R.id.recipeName);
-        //recipeNameToChange.setText(recipeName);
 
         //Button clicking methods
         toEdit();
         toDelete();
     }
 
+    //Click listener for Edit activity
     private void toEdit() {
         Button toEdit = (Button) findViewById(R.id.btnEdit);
         toEdit.setOnClickListener(new View.OnClickListener() {
@@ -53,16 +52,19 @@ public class ViewRecipe extends AppCompatActivity {
         });
     }
 
+    //Click listener for Delete activity
     private void toDelete() {
         Button toDelete = (Button) findViewById(R.id.btnDelete);
         toDelete.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 createAlert();
+                //Calls on method in façade class RecipeBook to delete the selected recipe by ID
                 rBook.deleteRecipe(getApplicationContext(), r.getId());
             }
         });
     }
 
+    //User alert to confirm or prevent deletion of recipe
     private void createAlert () {
         onPause();
         AlertDialog.Builder a_builder = new AlertDialog.Builder(this);
@@ -91,50 +93,60 @@ public class ViewRecipe extends AppCompatActivity {
 
 
     /*
-    View setters
+    View setters for layout fields
      */
+
+    //Sets the Title field of the recipe to be displayed to the stored value
     private void setTitle(Recipe recipe){
         String recipeTitle = recipe.getName();
         TextView resultsTitle = (TextView) findViewById(R.id.recipeName);
         resultsTitle.setText(recipeTitle);
     }
 
+    //Sets the Serving field of the recipe to be displayed to the stored value
     private void setServing(Recipe recipe){
         String recipeServing = String.valueOf(recipe.getNumServings());
         TextView resultsServing = (TextView) findViewById(R.id.txtviewRecipeServingsNumber);
         resultsServing.setText(recipeServing);
     }
 
+    //Sets the Preparation field of the recipe to be displayed to the stored value
     private void setPrep(Recipe recipe){
         String recipePrep = recipe.getPrepTime();
         TextView resultsPrep = (TextView) findViewById(R.id.txtviewRecipePrepNumber);
         resultsPrep.setText(recipePrep);
     }
 
+    //Sets the Cooking Time field of the recipe to be displayed to the stored value
     private void setCookTime(Recipe recipe){
         String recipeCookTime = recipe.getCookTime();
         TextView resultsCookTime = (TextView) findViewById(R.id.txtviewCookTimeNumber);
         resultsCookTime.setText(recipeCookTime);
     }
 
+    //Sets the Type field of the recipe to be displayed to the stored value
     private void setType(Recipe recipe){
         String recipeType = recipe.getType();
         TextView resultsType = (TextView) findViewById(R.id.viewRecipeTypeInfo);
         resultsType.setText(recipeType);
     }
 
+    //Sets the Calories field of the recipe to be displayed to the stored value
     private void setCalories(Recipe recipe){
         String recipeCalories = String.valueOf(recipe.getNumCalories());
         TextView resultsCalories = (TextView) findViewById(R.id.viewRecipeCaloriesInfo);
         resultsCalories.setText(recipeCalories);
     }
 
+    //Sets the Category field of the recipe to be displayed to the stored value
     private void setCategory(Recipe recipe){
         String recipeCategory = recipe.getCategory();
         TextView resultsCategory = (TextView) findViewById(R.id.viewRecipeCategoryInfo);
         resultsCategory.setText(recipeCategory);
     }
 
+    //Sets the Ingredients field of the recipe to be displayed to the stored values
+    //Converts the IngredientMeasure objects to String for display purposes
     private void setIngredients(Recipe recipe){
         ArrayList<IngredientMeasure> recipeIngredients = recipe.getIngredientMeasures();
         LinearLayout ll = (LinearLayout) findViewById(R.id.viewRecipeIngredientsInfo);
@@ -146,6 +158,10 @@ public class ViewRecipe extends AppCompatActivity {
         }
     }
 
+    /*
+    * Sets the Directions field of the recipe to be displayed to the stored values
+    * @param recipe Recipe to be displayed
+     */
     private void setDirections(Recipe recipe){
         ArrayList<String> recipeDirections = recipe.getDirections();
         LinearLayout ll = (LinearLayout) findViewById(R.id.viewRecipeDirectionsInfo);
@@ -157,6 +173,10 @@ public class ViewRecipe extends AppCompatActivity {
         }
     }
 
+    /*
+    * Master method to call every view setters
+    * @param recipe Recipe to be displayed
+     */
     private void masterView(Recipe recipe){
         setTitle(recipe);
         setServing(recipe);
